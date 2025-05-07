9ba85b77680edbeda15ce71f159bdd0bfdb30c43
Rejman <s.rejman@uu.nl>
2025-05-07 19:57:28 +0200

    svgs are transparent, Liberation sans on linux, notebook fixes

--- utils/general.py
+++ utils/general.py
@@ -1,9 +1,22 @@
 from matplotlib import pyplot as plt
-
+import matplotlib.pyplot as plt
+import platform
 
 def set_plot_params():
-    font = {'family' : 'Arial',
-            'weight' : 'normal',
-            'size'   : 7}
+    system = platform.system()
+    
+    if system == 'Windows':
+        font_family = 'Arial'
+    elif system == 'Linux':
+        font_family = 'Liberation Sans'
+    else:
+        font_family = 'sans-serif'  # Fallback
+    
+    font = {
+        'family': font_family,
+        'weight': 'normal',
+        'size': 7
+    }
+    
     plt.rc('font', **font)
-    plt.rcParams['figure.figsize'] = [8.8/2.54,6.22/2.54]
+    plt.rcParams['figure.figsize'] = [8.8 / 2.54, 6.22 / 2.54]
