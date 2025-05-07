00c3a4d30ac7f26c55a7d4dc8ba0f6e5b96a6ca4
Rejman <s.rejman@uu.nl>
2025-05-07 03:16:24 +0200

    added up to mass balances

---
+++ utils/pyGCMS/gcgc_utils.py
@@ -0,0 +1,179 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+import scipy.integrate as integrate
+import matplotlib.colors as colors
+
+
+
+
+
+def parse_chromatogram(path): #from a CSV file
+    df = pd.read_csv(path, sep = ',',  skiprows = 1)
+    df.columns = ['Time(ms)', 'Time(min)', 'unknown','Absolute Intensity']
+    df['Ret.Time'] = df['Time(min)']
+    df['Ret.Time']-=df['Ret.Time'][0]
+    df['Ret.Time[s]']=df['Ret.Time']*60
+    df.drop(['Time(ms)','Time(min)','unknown'], axis=1, inplace=True)
+    return df
+
+
+def split_solvent(df, solvent_time):
+    #set absoluyte intensity to 0 when time is < solvent_time
+    df.loc[df['Ret.Time[s]'] <= solvent_time, 'Absolute Intensity'] = 0
+    return df
+
+def add_split(df,split_time,sampling_interval):#split time in s, sampling interval in ms
+    rows_splitting = split_time/sampling_interval*1000
+    df['split_no_fromindex'] = df.index//rows_splitting 
+    while len(df[df['split_no_fromindex']==df['split_no_fromindex'].max()]) < len(df[df['split_no_fromindex']==df['split_no_fromindex'].min()]):
+        df.loc[len(df)+1] = df.iloc[-1]
+    return df
+
+def min_correct(df): # 'global minimum' as baseline
+    df['Absolute Intensity'] = df['Absolute Intensity'] - df['Absolute Intensity'].min()
+    return df
+
+def baseline_stridewise(df_array): #minimum in each stride as baseline
+    df_array = df_array - df_array.min(axis=0)
+    return df_array
+
+def convert_to2D(df,split_time):
+    df_short = df[['split_no_fromindex','Absolute Intensity']]
+    array_list = []
+    
+    for i in range(0,int(df_short['split_no_fromindex'].max())):
+        array_list.append(df_short[df_short['split_no_fromindex']==i]['Absolute Intensity'].values)
+
+    #turn arraylist into an 2D array
+    array = np.zeros((len(array_list),len(array_list[0])))
+    for i in range(0,len(array_list)):
+        array[i,:] = array_list[i]
+
+    index_list_retention_time = []
+    for i in range(len(array_list)):
+        index_list_retention_time.append(i*split_time)
+
+    columns_splittime = []
+    for i in range(len(array_list[0])):
+        columns_splittime.append(round(i*split_time/len(array_list[0]),3))
+
+    df_array = pd.DataFrame(array, index = index_list_retention_time, columns = columns_splittime)
+    df_array= df_array.T
+    df_array = df_array.iloc[::-1]
+    return df_array
+
+def shift_phase(df_array, shift):
+    df_array_shifted = np.roll(df_array, shift, axis=0)
+    return df_array_shifted
+
+import tifffile
+
+
+
+
+
+def normalize_array(df_array):
+    arrray_for_integral = np.array(df_array)
+    #integrate over rows
+    row_integrated_pre = integrate.trapz(arrray_for_integral, axis=0)
+    #integrating the new array
+    integral_non_norm = integrate.trapz(row_integrated_pre, axis=0)
+    df_array_norm = df_array/integral_non_norm
+    return df_array_norm
+    
+def integrate_masked(df_norm_array, maskpath):
+    mask =  tifffile.imread(maskpath)/255 # if the mask is binary no need to divide by 255
+    df_norm_array_masked  = df_norm_array*mask
+
+    array_norm_mask_diarom = np.array(df_norm_array_masked)
+    #integrate over rows
+    row_integrated = integrate.trapz(array_norm_mask_diarom, axis=0)
+    #integrating the new array
+    column_integrated = integrate.trapz(row_integrated, axis=0)
+    return column_integrated
+    
+
+
+def mask_integrate(df_array_norm, mask_dir):
+    mask_list = glob.glob(mask_dir + '*.tif')
+    #get the mask names
+    mask_names = []
+    for i in range(len(mask_list)):
+        mask_name = mask_list[i].split('\\')[-1].split('.')[0]
+        #split off the 'Mask_' part
+        mask_names.append(mask_name.split('Mask_')[-1])
+
+
+    integral_list = []
+    for i in range(len(mask_list)):
+        integral_list.append(integrate_masked(df_array_norm, mask_list[i]))
+
+    # make a dataframe with the integral values and the mask_names as column names
+    df_integral = pd.DataFrame([integral_list], columns = mask_names)
+    df_integral['unassigned'] = 1-df_integral[mask_names].sum(axis=1)
+    return df_integral
+
+def process_chromatogram(filepath, split_time, sampling_interval, mask_dir,shift=0, solvent_time=0):
+    df = parse_chromatogram(filepath)
+    df = split_solvent(df, solvent_time)
+    df = add_split(df,split_time,sampling_interval)
+    df_array = convert_to2D(df,split_time)
+    df_array  = baseline_stridewise(df_array)
+    df_array = shift_phase(df_array, shift)
+    df_array_norm = normalize_array(df_array)
+    df_integral = mask_integrate(df_array_norm, mask_dir)
+    return df_integral, df_array_norm
+
+def plot_2Dchromatogram(df_array_norm,maskdir,savedir,plotmask=True,title = '2D Chromatogram',split_time = 20):
+    mask_list = glob.glob(maskdir + '*.tif')
+    plt.figure(figsize=(8,8/1.615))
+    plt.imshow(np.sqrt(df_array_norm), cmap='viridis', interpolation='nearest', extent=[0, 106, 0, split_time], aspect='auto')
+    plt.colorbar(label='$\sqrt{\mathrm{intensity}}$')
+
+    colormaplist = [colors.ListedColormap(['none', 'C'+str(i)]) for i in range(6) ]
+    annotations = [['Alkanes/Alkenes',28,7],['Monoaromatics',45,12],['Diaromatics',30,19],['Triaromatics',64,4.5],['Pyrenes',93,8]]
+    if plotmask:
+        for i in range(len(mask_list)):
+            mask =  tifffile.imread(mask_list[i])/255 # if the mask is binary no need to divide by 255
+            plt.imshow(mask, cmap=colormaplist[i], interpolation='nearest', extent=[0, 106, 0, split_time], aspect='auto', alpha=0.2)
+            plt.text(annotations[i][1], annotations[i][2], annotations[i][0], fontsize=8, color='white')
+
+    plt.xlabel('Retention time 1 (min)')
+    plt.ylabel('Retention time 2 (s)')
+    plt.title(title)
+    plt.savefig(savedir, dpi=1000, bbox_inches='tight')
+
+def process_FID(filepath, split_time, sampling_interval,shift=0, solvent_time=0):
+    df = parse_chromatogram(filepath)
+    df = split_solvent(df, solvent_time)
+    df = add_split(df,split_time,sampling_interval)
+    df_array = convert_to2D(df,split_time)
+    df_array  = baseline_stridewise(df_array)
+    df_array = shift_phase(df_array, shift)
+    df_array_norm = normalize_array(df_array)
+    return df_array_norm
+
+
+
+
+
+
+
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+def baseline_als(y, lam, p, niter=50):
+    L = len(y)
+    D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+    D = lam * D.dot(D.transpose()) # Precompute this term since it does not depend on `w`
+    w = np.ones(L)
+    W = sparse.spdiags(w, 0, L, L)
+    for i in range(niter):
+        W.setdiag(w) # Do not create a new matrix, just update diagonal values
+        Z = W + D
+        z = spsolve(Z, w*y)
+        w = p * (y > z) + (1-p) * (y < z)
+    return z
---
+++ utils/pyGCMS/utils.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import numpy as np
+from scipy import integrate
+import glob
+def get_meta(dir_path):
+    meta_frame = pd.read_csv(filepath_or_buffer=dir_path+'meta.csv', sep=',',index_col=0,
+                            skip_blank_lines=True, names=['item','value'],
+                            usecols=[7, 8])
+    return meta_frame
+
+def get_calib_factors(meta_frame):
+    N2_calibs = []
+    H2_calibs = []
+    TCD_HC_calibs = []
+    FID_HC_calibs = []
+    injection = []
+    for i in range(14): # 14 sample loops in total
+        injection.append(i+2)
+
+        cellname_N2="Calibration factor TCD_N2_Inj{}"
+        cellname_N2.format(i+2) #we start with injection no.2, counting from 1
+        N2_calibs.append(float(meta_frame['value'][cellname_N2.format(i+2)]))
+
+        cellname_H2="Calibration factor TCD_H2_Inj{}"
+        cellname_H2.format(i+2)
+        H2_calibs.append(float(meta_frame['value'][cellname_H2.format(i+2)]))
+
+        cellname_HC_TCD="Calibration factor TCD_HC_Inj{}"
+        cellname_HC_TCD.format(i+2)
+        TCD_HC_calibs.append(float(meta_frame['value'][cellname_HC_TCD.format(i+2)]))
+
+        cellname_HC="Calibration factor FID_Inj{}"
+        cellname_HC.format(i+2)
+        FID_HC_calibs.append(float(meta_frame['value'][cellname_HC.format(i+2)]))
+    # convert the 3 lists to a dataframe with injection number as index
+    calib_factors = pd.DataFrame({'N2': N2_calibs, 'H2': H2_calibs,'HC_TCD':TCD_HC_calibs, 'HC_FID': FID_HC_calibs}, index=injection)
+    calib_factors.loc[1] = [10,10,10,10]
+    calib_factors = calib_factors.sort_index()
+    return calib_factors
+
+
+def integrate_TCD_chromatogram(chrom_path,compound_frame_TCD):
+    chromatogram = pd.read_csv(filepath_or_buffer=chrom_path, sep='\t',index_col=0,skiprows=42,thousands=r',')
+    chromatogram = chromatogram.drop(columns=['Step (s)'])
+    integrals = []
+    for i in compound_frame_TCD.index.to_list():
+        index_bl = np.abs(((chromatogram.index - compound_frame_TCD['baseline_point'].loc[i]).to_numpy())).argmin()
+        chrom_bl = chromatogram - chromatogram.iloc[index_bl] #baseline_correction at selected point
+        x = chrom_bl.index[(chrom_bl.index > compound_frame_TCD['lower_bound'].loc[i]) & (chrom_bl.index < compound_frame_TCD['upper_bound'].loc[i])]
+        y = chrom_bl['Value (mV)'].loc[x]
+        integrals.append(integrate.trapz(y,x))
+    return integrals, chromatogram
+
+def integrate_TCD_chromatogram_new(chrom_path,compound_frame_TCD):
+    chromatogram = pd.read_csv(filepath_or_buffer=chrom_path, sep='\t',index_col=0,skiprows=42,thousands=r',')
+    chromatogram = chromatogram.drop(columns=['Step (s)'])
+    integrals = []
+    for i in compound_frame_TCD.index.to_list():
+        chrom_bl = chromatogram - chromatogram.iloc[chromatogram.index.get_loc(compound_frame_TCD['baseline_point'].loc[i])]
+        x = chrom_bl.index[(chrom_bl.index > compound_frame_TCD['lower_bound'].loc[i]) & (chrom_bl.index < compound_frame_TCD['upper_bound'].loc[i])]
+        y = chrom_bl['Value (mV)'].loc[x]
+        bl_slope = (y.iloc[-1] - y.iloc[0])/(x[-1] - x[0])
+        y = y - (bl_slope*(x-x[0])+y.iloc[0])
+
+        integrals.append(integrate.trapz(y,x))
+    return integrals, chrom_bl
+
+
+
+
+def get_TCD_flows(filepath, compound_frame_TCD):
+    
+    meta_frame = get_meta(filepath)
+    calib_factors = get_calib_factors(meta_frame)
+    calib_factors.loc[1] = [100,100,100,100] # blank row to not mess with changing indeces all the time
+    calib_factors = calib_factors.sort_index()
+
+    molar_flow = 2.018104822/50*float(meta_frame['value']['N2 flow rate: [ml/min]']) #mmol/min. 2.018 is the molar amount of N2 in 50ml at 25C and 10**5 Pa
+    init = float(meta_frame['value']['GC start time:'])  # start time of filling the loops (min)
+    inj_time = float(meta_frame['value']['Time sampling loop: [min]']) # time of recording of 1 injection in min
+
+    filelist_TCD = glob.glob(filepath+'*TCD.txt*')
+    filelist_TCD.sort(key=len)
+    integral_list = []
+
+    for i in enumerate(filelist_TCD):
+        integrals, chromatogram = integrate_TCD_chromatogram(filelist_TCD[i[0]],compound_frame_TCD)
+        integral_list.append(np.array(integrals))
+        
+    TCD_frame = pd.DataFrame(integral_list, columns=compound_frame_TCD.index.to_list(),index=[i[0]+1 for i in enumerate(filelist_TCD)])
+    #first row to nan
+    TCD_frame.loc[1] = np.nan
+
+    flow_frame_TCD_N2 = pd.DataFrame(data=molar_flow/(np.array(TCD_frame['Nitrogen']) / np.array(calib_factors['N2'])/100), columns=['Nitrogen flow'], index=TCD_frame.index)
+
+    H2_flow = np.array(TCD_frame['Hydrogen']) / np.array(calib_factors['H2'])/100 * flow_frame_TCD_N2['Nitrogen flow']
+    TCD_frame.drop(columns=['Nitrogen','Hydrogen'], inplace=True)
+
+    #multiply all the columns with flow_frame_TCD_N2_2
+    TCD_frame = TCD_frame.multiply(flow_frame_TCD_N2['Nitrogen flow'], axis=0)
+    #devide every column by an array
+    for col in TCD_frame.columns:
+        TCD_frame[col] = TCD_frame[col] / np.array(calib_factors['HC_TCD'])/100
+    TCD_frame['Hydrogen'] = H2_flow
+    TCD_frame['N2_corr'] = flow_frame_TCD_N2['Nitrogen flow']
+    TCD_frame.index = (TCD_frame.index-1)*inj_time+init
+    return TCD_frame 
+
+
+
+
+def process_chromatogram(chrom_path,peaklist_path,time):
+    chromatogram = pd.read_csv(filepath_or_buffer=chrom_path, sep='\t',index_col=0,skiprows=42,thousands=r',')
+    chromatogram = chromatogram.drop(columns=['Step (s)'])
+    baseline = chromatogram['Value (pA)'].min() # left as is to allow different baseline
+    chromatogram['bl_substracted'] = chromatogram['Value (pA)'] - baseline
+    peaklist = pd.read_csv(filepath_or_buffer=peaklist_path, sep=',',index_col=0)
+    integrals = []
+    labels = peaklist.index.tolist()
+    for i in range(len(peaklist)):
+        integrals.append(integrate.trapz(chromatogram['bl_substracted'][(chromatogram.index>peaklist['lower_bound'].iloc[i]) & (chromatogram.index<peaklist['upper_bound'].iloc[i])],chromatogram.index[(chromatogram.index>peaklist['lower_bound'].iloc[i]) & (chromatogram.index<peaklist['upper_bound'].iloc[i])]))
+    # append an integral of the total chromatogram
+    labels.append('total')
+    integrals.append(integrate.trapz(chromatogram['bl_substracted'],chromatogram.index))
+    integral_frame_pA = pd.DataFrame({time: integrals}, index=labels)
+    return chromatogram, integral_frame_pA
+
+
+def get_integral_frame(filelist_FID, Peaklist_path, inj_time_timelist):
+    integral_list = []
+    for i in range(len(filelist_FID)):
+        chromatogram, integral = process_chromatogram(filelist_FID[i],Peaklist_path,inj_time_timelist[i])
+        integral_list.append(integral)
+
+    integral_frame_FID = pd.concat(integral_list, axis=1)
+    return integral_frame_FID
+
+
+
+
+
+def calc_molarCflow(integral_frame_FID_pA, calib_factors, flow_frame_TCD):
+    #dividing by calibration factor and adjusting for N2 flow
+    integral_frame_FID_mol = integral_frame_FID_pA / np.array(calib_factors['HC_FID'])[1:]/100 * flow_frame_TCD['N2_corr']
+    return integral_frame_FID_mol.fillna(0)
+
+
+def get_indiv_integrals(integral_frame_FID_mol, inj_time_timelist): # rerun
+    integral = integrate.trapz(integral_frame_FID_mol,inj_time_timelist)
+    integral_frame = pd.DataFrame(integral, columns=['molar'], index=integral_frame_FID_mol.index)
+    integral_frame['mass'] = integral_frame['molar']*14/1000
+    return integral_frame
+
+def get_TCD_masses(integral_frame_TCD_mol, inj_time_timelist):
+    dfT=integral_frame_TCD_mol.T.fillna(0)
+    integral = integrate.trapz(dfT,inj_time_timelist)
+    integral_frame = pd.DataFrame(integral, columns=['molar_TCD'], index=dfT.index)
+    integral_frame['mass_TCD'] = integral_frame['molar_TCD']*[44,42,2]/1000
+    return integral_frame
+
+
+def get_liquid_yield(meta_frame):
+    return float(meta_frame['value']['Mass of liquid sample: [g]'])
+
+
+### TGA Section
+
+def get_coke_amount(homedir, cat_mass = 0.625):
+    try:
+        TGA_dir = homedir + '/TGA_burnoff/'
+        TGA_list = glob.glob(TGA_dir + '*.txt')
+        TGA_file = TGA_list[0]
+    except:
+        print('No TGA file found for ' + homedir)
+        return 'No TGA file found'
+    names = ['Blank', 'Time', 'Unsubtracted weight', 'Baseline weight',
+        'Program Temp.','Sample Temp.', 'Sample Purge Flow',
+        'Balance purge flow']
+    frame_full = pd.read_table(TGA_file,sep='\t', skiprows=98,header=None, names=names, engine='python', skipfooter=45)
+    startline = frame_full[frame_full['Sample Temp.'] >= 150].index[0]
+    endline = frame_full[frame_full['Sample Temp.'] >= 800].index[0]
+    frame = frame_full.iloc[(startline-40):endline,:]
+    min_mass = min(frame['Unsubtracted weight'])
+    max_mass = max(frame['Unsubtracted weight'])
+    percent_loss = (max_mass-min_mass)/max_mass*100
+
+    total_coke = percent_loss/100*max_mass*cat_mass/min_mass
+    return total_coke
+
+
+
+
+def flatten_export(masses_export,cat): 
+    new = masses_export.drop(['molar_TCD','molar'],axis = 1)
+    new.drop(['Propane','Propylene','total'], inplace=True)
+    new.sum(axis=1).to_frame(name=cat).T
+    return new.sum(axis=1).to_frame(name=cat).T
